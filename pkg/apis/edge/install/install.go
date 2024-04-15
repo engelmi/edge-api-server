68419945c3473abcfdcee558e70b2a4a98fe8256
Michael Engel <mengel@redhat.com>
2024-04-15 13:44:35 +0200

    Install v1alpha1 to scheme
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- pkg/apis/edge/install/install.go
+++ pkg/apis/edge/install/install.go
@@ -3,9 +3,15 @@
 package edge
 
 import (
+	"github.com/engelmi/edge-api-server/pkg/apis/edge"
+	"github.com/engelmi/edge-api-server/pkg/apis/edge/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 )
 
 // Install registers the API group and adds types to a scheme
 func Install(scheme *runtime.Scheme) {
+	utilruntime.Must(edge.AddToScheme(scheme))
+	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+	utilruntime.Must(scheme.SetVersionPriority(v1alpha1.SchemeGroupVersion))
 }
